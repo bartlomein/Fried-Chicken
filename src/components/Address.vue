bbc3ebf8357ebe2d7bf93a43b8efa3f146aad9f6
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-07-05 17:06:51 -0400

    got google autocomplete working

--- src/components/Address.vue
+++ src/components/Address.vue
@@ -1,31 +1,57 @@
 <template>
-  <div class="address">
-   TESTING !!!!
-  </div>
+ <input ref="autocomplete" 
+        placeholder="Search" 
+        class="search-location"
+        onfocus="value = ''" 
+        type="text" />
+
+
 </template>
 
 <script>
 export default {
-  props: {
-    address:null
+  mounted() {
+    this.autocomplete = new google.maps.places.Autocomplete(
+      (this.$refs.autocomplete),
+      {types: ['geocode']}
+    );
+    this.autocomplete.addListener('place_changed', () => {
+      let place = this.autocomplete.getPlace();
+      let ac = place.address_components;
+      let lat = place.geometry.location.lat();
+      let lon = place.geometry.location.lng();
+      let city = ac[0]["short_name"];
+      console.log(`The user picked ${city} with the coordinates ${lat}, ${lon}`);
+    });
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+*, *::after, *::before {
+  margin: 0;
   padding: 0;
+  box-sizing: inherit;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+body {
+  background-color: #dcdde1;
+  color: #2f3640;
+  padding: 3rem;
 }
-a {
-  color: #42b983;
+
+.search-location {
+  display: block;
+  width: 60vw;
+  margin: 0 auto;
+  margin-top: 5vw;
+  font-size: 20px;
+  font-weight: 400;
+  outline: none;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  border-radius: 10px;
 }
 </style>
