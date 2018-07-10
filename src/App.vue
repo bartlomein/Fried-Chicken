cf7b8d10f17bc129da3efc4ede14ec3f27468268
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-07-09 21:54:59 -0400

    kinda working gettign titles

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,14 @@
   <div id="app">
     <Address @send-location="updateLocation"></Address>
 
-    <div v-if="place">
-      Place {{place}}
-    </div>
+    
+    <div id="results"></div>
   </div>
 </template>
 
 <script>
 import Address from './components/Address.vue';
-import axios from 'axios';
+
 
 export default {
   name: 'app',
@@ -30,6 +29,32 @@ export default {
       this.place = googlePlace;
       this.lat = this.place.geometry.location.lat();
       this.lon = this.place.geometry.location.lng();
+  
+       var request = {
+    location: new google.maps.LatLng(this.lat, this.lon),
+    radius: '50000',
+    keyword: 'boof'
+};
+
+    var container = document.getElementById('results');
+
+      var service = new google.maps.places.PlacesService(container);
+      service.nearbySearch(request, callback);
+
+
+    function callback(results, status) {
+      console.log(results)
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+        for (var i = 0; i < results.length; i++) {
+
+            container.innerHTML += results[i].name + '<br />';
+        }
+    }
+}
+
+
+
       console.log(this.lat,this.lon)
     //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
 
