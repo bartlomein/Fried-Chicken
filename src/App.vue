4c360b8152efd53f18cb22fad874d20d9784a2d3
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-07-12 16:33:36 -0400

    detail works bitch

--- src/App.vue
+++ src/App.vue
@@ -55,9 +55,11 @@ export default {
       function callback(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
           vm.placesArray = results;
+          console.log(vm.placesArray)
           
         }
       }
+      
 
       //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
     },
--- src/components/Spots.vue
+++ src/components/Spots.vue
@@ -1,25 +1,68 @@
 <template>
     <div id = "spot-container">
         Name:{{places.name}}<br>
-        <button @click="callModal"></button>
+        <div id="results-one"></div>
+        <button @click="callModal(), updatePlace(places)"></button>
+        
 
-
-    <sweet-modal ref="modal">This is an alert.{{places.rating}}</sweet-modal>
+    <div id = "modal-container">
+    <sweet-modal  ref="modal" :title="places.name">
+        
+        {{places.rating}}
+        <div id = "details" v-if="detailedPlace">
+            {{detailedPlace.formatted_phone_number}}
+        </div>
+        
+        </sweet-modal>
     </div>
-
+</div>
 </template>
 
 <script>
 export default {
-    props: ['places'],
-    methods:{
-        callModal(){
-            this.$refs.modal.open();
+  props: ["places"],
+  data() {
+    return {
+      placeId: '',
+      detailedPlace: null
+    };
+  },
+  methods: {
+    callModal() {
+      this.$refs.modal.open();
+    },
+    updatePlace(places) {
+      this.placeId = places.place_id;
+      console.log(this.placeId);
+      let vm = this;
+      
+      let request = {
+        placeId: this.placeId,
+        fields: ["name", "rating", "formatted_phone_number"]
+      };
+      let container = document.getElementById("results-one");
+      let service = new google.maps.places.PlacesService(container);
+
+      
+
+
+      service.getDetails(request, callback);
+
+      function callback(place, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            
+
+          vm.detailedPlace = place;
+          console.log(vm.detailedPlace)
+
         }
+      }
+      console.log(vm.detailedPlace)
     }
-}
+  },
+  mounted() {}
+};
 </script>
 
 <style>
-
 </style>
