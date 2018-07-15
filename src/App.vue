bffa0d8c6fcfc42d6002467b0d3858c911049e7a
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2018-07-15 19:35:58 -0400

    workking on combined map, for some reason google example not working right

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <div id="map">
+      </div>
     <div id = "search">
       <Address @send-location="updateLocation"></Address>
     </div>
@@ -14,16 +16,14 @@
 </template>
 
 <script>
-
 import Address from "./components/Address.vue";
 import Spots from "./components/Spots.vue";
-
 
 export default {
   name: "app",
   components: {
     Address,
-    Spots,
+    Spots
   },
   data() {
     return {
@@ -34,6 +34,7 @@ export default {
       showDetail: false
     };
   },
+  mounted() {},
   methods: {
     updateLocation(googlePlace) {
       this.place = googlePlace;
@@ -44,7 +45,7 @@ export default {
         location: new google.maps.LatLng(this.lat, this.lon),
         radius: "3000",
         keyword: "chicken",
-        rankby:"distance"
+        rankby: "distance"
       };
 
       let container = document.getElementById("results");
@@ -56,26 +57,51 @@ export default {
       function callback(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
           vm.placesArray = results;
-          console.log(vm.placesArray)
-          
+          console.log(vm.placesArray);
+          vm.toggleDetail();
         }
       }
-      
 
       //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
     },
-    toggleResults(){
-      this.showResults = !this.showResults;
-      
-    },
-    toggleDetail(){
-      this.showDetail = !this.showDetail;
-      console.log(this.showDetail)
-    },
-  },
-  watch: {
-    // whenever question changes, this function will run
-    place: function() {}
+    toggleDetail() {
+      var markers = [];
+      let vm = this;
+      function initialize() {
+        var beaches = vm.placesArray
+
+        var map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 12,
+          center: new google.maps.LatLng(vm.place.geometry.location.lat(), vm.place.geometry.location.lng()),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+
+        for (var i = 0; i < vm.placesArray.length; i++) {
+          var newMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(beaches[i][1], beaches[i][2]),
+            map: map,
+            title: beaches[i][0]
+          });
+
+          google.maps.event.addListener(
+            newMarker,
+            "click",
+            (function(newMarker, i) {
+              return function() {
+                infowindow.setContent(beaches[i][0]);
+                infowindow.open(map, newMarker);
+              };
+            })(newMarker, i)
+          );
+
+          markers.push(newMarker);
+        }
+      }
+
+      initialize();
+    }
   }
 };
 </script>
@@ -88,5 +114,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#map {
+  height: 400px;
+  width: 400px;
 }
 </style>
--- src/components/Spots.vue
+++ src/components/Spots.vue
@@ -2,7 +2,7 @@
     <div id = "spot-container">
         Name:{{places.name}}<br>
         <div id="results-one"></div>
-        <button @click="callModal(), updatePlace(places), callMap()" ></button>
+        <button @click="callModal(), updatePlace(places)" ></button>
         
 
     <div id = "modal-container">
@@ -16,7 +16,8 @@
         </div>
         </sweet-modal-tab>
         <sweet-modal-tab title="Reviews" id="tab2" >TEST</sweet-modal-tab>
-        <sweet-modal-tab title="Map" id="tab3"  ><div id="map">      
+        <sweet-modal-tab title="Map" id="tab3">
+          <div id="map">      
           
           </div></sweet-modal-tab>
         
@@ -37,14 +38,20 @@ export default {
       phone: null
     };
   },
-  mounted() {},
   methods: {
     callModal() {
       this.$refs.modal.open();
+      this.places
+      
+    },
+    refreshMap(){
+
     },
 
+
+
     callNumber(phone) {
-      console.log(phone);
+
       let newphone = phone
         .replace("(", "")
         .replace(")", "")
@@ -52,48 +59,8 @@ export default {
         .replace("-", "");
       newphone = "tel:+1" + newphone;
 
-      console.log(newphone);
+    
       window.open(newphone);
-    },
-    callMap(places) {
-      let vm = this;
-      console.log(this.places);
-      console.log("callmap start")
-      var map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: this.places.geometry.location.lat(), lng: this.places.geometry.location.lng() },
-        zoom: 15
-      });
-
-      var infowindow = new google.maps.InfoWindow();
-      var service = new google.maps.places.PlacesService(map);
-      console.log("before get details")
-      service.getDetails(
-        {
-          placeId: this.placeId
-        },
-        function(place, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            var marker = new google.maps.Marker({
-              map: map,
-              position: place.geometry.location
-            });
-            google.maps.event.addListener(marker, "click", function() {
-              infowindow.setContent(
-                "<div><strong>" +
-                  place.name +
-                  "</strong><br>" +
-                  "Place ID: " +
-                  place.place_id +
-                  "<br>" +
-                  place.formatted_address +
-                  "</div>"
-              );
-              console.log("before map open")
-              infowindow.open(map, this);
-            });
-          }
-        }
-      );
     },
     updatePlace(places) {
       console.log(places);
@@ -112,6 +79,8 @@ export default {
           "review"
         ]
       };
+
+      
       let container = document.getElementById("results-one");
       let service = new google.maps.places.PlacesService(container);
 
@@ -129,7 +98,7 @@ export default {
 </script>
 
 <style scoped>
-#map {
+.map {
   height: 500px;
   width: 500px;
 }
